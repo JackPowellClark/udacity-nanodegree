41e3a1ae9171e30a57fe6cba0c4f32de143e0500
JackPowellClark <jackpowellclark@gmail.com>
2016-11-13 21:41:04 +0000

    Add information to resume

--- project-4-online-resume/js/resumeBuilder.js
+++ project-4-online-resume/js/resumeBuilder.js
@@ -1,3 +1,69 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*jshint esversion: 6 */
+var bio = {
+  name: "Jack Clark",
+  role: "Product Development",
+  // welcomeMessage: "Welcome!",
+  bioPic: "../images/profilePicture.jpg",
+  contacts: {
+    mobile: "+447963111196",
+    email: "jackpowellclark@gmail.com",
+    github: "jackpowellclark",
+    location: "London"
+  },
+  skills: ["HTML5", "CSS3", "JavaScript", "Ruby"]
+};
+
+var education = {
+  schools: [
+    {
+      name: "University of Sussex",
+      subject: "Technology and Innovation Management",
+      degree: "MSc",
+      result: "Distinction",
+      date: "Sep. 2014 — Sep. 2015",
+      modules: ["Managing Innovation", "Science, Technology and Innovation: Markets, Firms and Policies",
+        "Information and Communication Technologies: Policy and Strategy", "Managing Intellectual Property",
+        "Technology Management Project", "Introduction to Statistical Research Methods"]
+    },
+    {
+      name: "University of Sussex",
+      subject: "Computer Science (specialism in Music Informatics)",
+      degree: "BSc",
+      result: "First Class Honours",
+      date: "Oct. 2011 — Jun. 2014",
+      modules: ["Further Programming", "Machine Learning", "Acquired Intelligence and Adaptive Behaviour",
+        "Web Computing", "Natural Language Engineering", "Databases", "Technology-Enhanced Learning Environments",
+        "Intelligence in Animals and Machines", "Generative Creativity"]
+    }
+  ],
+  onlineCourses: [
+    {
+      school: "Udacity",
+      title: "Front-End Web Developer",
+      dates: "Jul. 2016 — Present"
+    }
+  ]
+};
+
+var work = {
+  jobs: [
+    {
+      employer: "The Engine Group (Partners Andrews Aldridge)",
+      title: "Product Development",
+      dates: "Oct. 2015 – Present",
+      description: `Based within the Technology and Innovation Team, this role looks to bridge\
+        business, experience design, and engineering; guiding new product ideas from initial\
+         concept, to launch.`
+    }
+  ]
+};
+
+var projects = {
+  projects: [
+    {
+      title: "Experience Lab (Website)",
+      dates: "Nov. 2016",
+      description: "Tasked with X, Y, Z."
+    }
+  ]
+};
