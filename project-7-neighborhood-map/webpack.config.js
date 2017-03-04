2ec7ce92671d9327cc38ad3c861b3914bd3083d2
Jack Clark <jackclark@Jacks-MacBook-Pro.local>
2017-03-04 13:59:15 +0000

    Commit tools

---
+++ project-7-neighborhood-map/webpack.config.js
@@ -0,0 +1,84 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const CONFIG = {
+  entry: './src/index.js',
+  output: {
+    path: Path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+          /* use style-loader in development */
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './src/index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash].css',
+      disable: process.env.NODE_ENV === 'development',
+    }),
+  ],
+  devServer: {
+    /* historyApiFallback: true,
+    noInfo: true, */
+    contentBase: Path.resolve(__dirname, 'src'),
+  },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new Webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    /* Minimize all JavaScript output of chunks */
+    new Webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    /* Switch loaders to minimize mode */
+    new Webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ]);
+}
+
+module.exports = CONFIG;
