43ea379a8d1b4a3b2f8fa0f9cd4d04f5a28fa436
Jack Clark <jackclark@jacks-mbp.home>
2017-03-11 13:11:34 +0000

    Refactor initMap to GoogleMapUtil

--- project-7-neighborhood-map/src/GoogleMapUtil.js
+++ project-7-neighborhood-map/src/GoogleMapUtil.js
@@ -1,24 +1,36 @@
-/* global google */
-
-/* Some information about GoogleMap  */
+/* Singleton class encapsulating helper methods for Google Maps */
 export default class GoogleMapUtil {
-  /* Given a map and array of marker data, add each and return all */
+
+  /* Create and return a new map */
+  static initMap(element, position) {
+    return new google.maps.Map(document.getElementById(element), {
+      center: position,
+      zoom: 16,
+      disableDefaultUI: true,
+      zoomControl: true,
+    });
+  }
+
+  /* Given a map and array of marker data, add each marker and return all */
   static drawMarkers(map, arrayOfMarkerData, infoWindow) {
     const markers = [];
 
+    /* Iterate over each marker in the given array of marker data */
     for (const markerInfo of arrayOfMarkerData) {
       const marker = this.createMarker(map, markerInfo.position,
         markerInfo.title, markerInfo.content, markerInfo.brews);
 
-      marker.showInfo = function () {
+      /* Give each marker a function to display information */
+      marker.displayInfo = function () {
         GoogleMapUtil.animateMarker(marker);
         GoogleMapUtil.populateInfoWindow(map, marker, infoWindow);
       };
-
+      /* Invoke the above function if marker is clicked */
       marker.addListener('click', () => {
-        marker.showInfo();
+        marker.displayInfo();
       });
 
+      /* Ensure each new marker is added to array 'markers' */
       markers.push(marker);
     }
     return markers;
@@ -43,7 +55,7 @@ export default class GoogleMapUtil {
     }, 700);
   }
 
-  /* Given a marker, create and popular an info window */
+  /* Given a marker, create and populate an info window */
   static populateInfoWindow(map, marker, infoWindow) {
     infoWindow.marker = marker;
     infoWindow.setContent(`
--- project-7-neighborhood-map/src/viewModel.js
+++ project-7-neighborhood-map/src/viewModel.js
@@ -1,43 +1,34 @@
 /* global google */
-/* import KnockoutJS and SlideoutJS as dependencies. */
-import Knockout from 'knockout';
+/* import KnockoutJS, GoogleMapUtil.js, and markerData as dependencies. */
+import KO from 'knockout';
 import GoogleMapUtil from './GoogleMapUtil';
 import markersData from './MarkerData.json';
 
-/* Create a new map */
-const map = new google.maps.Map(document.getElementById('map'), {
-  center: {
-    lat: 50.8237,
-    lng: -0.141,
-  },
-  zoom: 16,
-  disableDefaultUI: true,
-  zoomControl: true,
-});
-
+/* Create an initialised map and info window */
+const map = GoogleMapUtil.initMap('map', { lat: 50.8237, lng: -0.1385 });
 const infoWindow = new google.maps.InfoWindow();
-/* const markers = GoogleMapUtil.drawMarkers(map, markersData.markers, infoWindow); */
 
-/* Define a *viewmodel* for both the data and behavior of your UI */
+/* Define a viewmodel for both the data and behavior of the UI */
 function ViewModel() {
   const self = this;
 
-  /* Data */
-  self.filters = Knockout.observableArray(markersData.filters);
-  self.filter = Knockout.observable('');
-  self.myMarkers = Knockout.observableArray(GoogleMapUtil.drawMarkers(map,
+  /* Data (model) */
+  self.filter = KO.observable('');
+  self.filters = KO.observableArray(markersData.filters);
+  self.myMarkers = KO.observableArray(GoogleMapUtil.drawMarkers(map,
     markersData.markers, infoWindow));
-  self.filteredItems = Knockout.computed(() => {
+
+  self.filteredItems = KO.computed(() => {
     const filter = self.filter();
     /* If there's no filter currently set, or it's set to all, set all markers
      * visable and return */
     if (!filter || filter === 'All') {
-      Knockout.utils.arrayForEach(self.myMarkers(), (marker) => {
+      KO.utils.arrayForEach(self.myMarkers(), (marker) => {
         marker.setVisible(true);
       }); return self.myMarkers();
     }
     /* Iterate through markers, checking . Return a filtered list of cafes. */
-    return Knockout.utils.arrayFilter(self.myMarkers(), (marker) => {
+    return KO.utils.arrayFilter(self.myMarkers(), (marker) => {
       for (const brew of marker.brews) {
         if (brew === filter) { marker.setVisible(true); return true; }
       } marker.setVisible(false);
@@ -45,5 +36,5 @@ function ViewModel() {
   });
 }
 
-// Activates knockout.js
-Knockout.applyBindings(new ViewModel());
+/* Activate knockout.js */
+KO.applyBindings(new ViewModel());
