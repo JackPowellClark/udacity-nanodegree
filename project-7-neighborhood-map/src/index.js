73fb10ae79130d400d32544d41c2e27c0f27fa8f
Jack Clark <jackclark@jacks-mbp.home>
2017-03-11 13:11:43 +0000

    Comment typo

--- project-7-neighborhood-map/src/index.js
+++ project-7-neighborhood-map/src/index.js
@@ -1,4 +1,4 @@
-/* require both index.html and main.scss for Webpack */
+/* Require index.html, main.scss, and viewModel.js for Webpack */
 require('./index.html');
 require('./main.scss');
 require('./viewModel.js');
